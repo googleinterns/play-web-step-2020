b7b2dd45d9c7c598e8070c7b748d3a2822e9a887
Essence Cain <evcain@aggies.ncat.edu>
2020-06-26 19:56:37 +0000

    Fix formatting

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
     componentDidMount() {
-        console.log("Inside componentDidMount!");
         fetch("/api/v1/test-servlet")
-          .then((response) => response.text())
-          .then((text) => {
-        console.log("here is the text from servlet: ", text);
-      });
+            .then((response) => response.text())
+            .then((text) => {
+                console.log("here is the text from servlet: ", text);
+            });
     }
 
     render() {
         return (
-          <div className="App">
-            <div className="App-header">
-              <h2>Welcome to React</h2>
+            <div className="App">
+                <div className="App-header">
+                    <h2>Welcome to React</h2>
+                </div>
             </div>
-          </div>
         );
     }
 }
