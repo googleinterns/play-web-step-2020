35213ed9c2d8185b465a9849252920f400ceee80
Essence Cain <evcain@aggies.ncat.edu>
2020-06-29 18:52:38 +0000

    Modify App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { FetchMock } from '@react-mock/fetch';
 import App from './App';
+import { findRenderedComponentWithType } from 'react-dom/test-utils';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const renderComponent = ({ text }) =>
+render(
+    <FetchMock
+    mock={[
+        {matcher: '/api/v1/test-servlet', response: 200}
+    ]}
+    >
+    </FetchMock>
+);
+
+test('verifies fetch is called', async () => {
+    const { getByText } = findRenderedComponentWithType();
+    
+
+})
+
+test('loads and displays greeting', () => {
+    const { getByText } = render(<App />);
+    const textElement = getByText('Welcome to React');
+    expect(textElement).toBeInTheDocument();
 });
