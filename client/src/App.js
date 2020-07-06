e51eb5291dd9914fe77f59996e4e16753adedfd5
Essence Cain <evcain@aggies.ncat.edu>
2020-07-06 18:45:37 +0000

    Created tests for RecommendedCategories and CategoryContainer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import './App.css'
-
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import './App.css';
+import RecommendedCategories from './cluster/RecommendedCategories/RecommendedCategories.js';
 
 class App extends Component {
   constructor(props) {
@@ -21,9 +21,10 @@ class App extends Component {
           <h2>Welcome to React</h2>
           {this.state.text}
         </div>
+        <RecommendedCategories />
       </div>
     )
   }
 }
 
-export default App
+export default App;
