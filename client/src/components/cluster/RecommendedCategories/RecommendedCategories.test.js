61566bf1e86a742fd8d7c4537a4b0ee2775e584b
Essence Cain <evcain@aggies.ncat.edu>
2020-07-08 19:29:02 +0000

    Add deleted files to PR

--- client/src/components/cluster/RecommendedCategories/RecommendedCategories.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, wait } from '@testing-library/react';
-import RecommendedCategories from './RecommendedCategories.js';
-
-test('correct header', () => {
-    const{ getByText } = render(<RecommendedCategories />);
-    const textElement = getByText('Browse recommended categories');
-    expect(textElement).toBeInTheDocument();
-})
