01fa1a5cad1545157fec06f5b4b1e3c0a3c781c8
Essence Cain <evcain@aggies.ncat.edu>
2020-07-09 21:26:18 +0000

    Implement material design card component in RecommendedCategories

--- client/src/Clusters/RecommendedCategories/RecommendedCategories.js
+++ client/src/Clusters/RecommendedCategories/RecommendedCategories.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CategoryContainer from './CategoryContainer.js';
 import { listOne, listTwo } from './mockData.js';
+import '../styles.css';
 
 class RecommendedCategories extends Component {
     render() {
@@ -16,16 +17,17 @@ class RecommendedCategories extends Component {
 
         return (
             <div className="category-section">
-                <div className="section-header">
+                <div className="category-section-header">
                     <h2>Didn't find what you're looking for?</h2>
                     <a href="https://play.google.com/">Browse recommended categories</a>
                 </div>
 
                 {lists.map((value, index) => {
                     return (
-                        <div className="row" key={index}>
-                            <div className="horizontal-row">
+                        <div className="my-card" key={index}>
+                            <div className="my-card-content">
                                 {value.cards.map(makeRow)}
+
                             </div>
                         </div>
                     );
