19297d93138e9082843b0b9a88576f1aa1b9f6ac
Ayomide Adekunle <davidadekunle88@gmail.com>
2020-07-16 13:33:32 +0000

    Fixes comments.

--- client/src/Clusters/AppOfTheWeek/AppOfTheWeek.js
+++ client/src/Clusters/AppOfTheWeek/AppOfTheWeek.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import '../styles.css';
-import './AOTWStyle.css'
+import './AppOfTheWeek.style.css'
 import {aotw} from './mockData.js';
 
 class AppOfTheWeek extends Component {
     render(){
         return(
-            <div className="aotw">
+            <div className="app-of-the-week">
                 <div className="text">
-                    <h1 className="aotw-header">{aotw.title}</h1>
-                    <p className="aotw-description">{aotw.description}</p>
+                    <h2 className="header">{aotw.title}</h2>
+                    <p className="description">{aotw.description}</p>
                     <img className="icon-img" alt="Strata Icon" src={aotw.icon} />
                     <h3 className="app-name">{aotw.app_name}</h3>
                     <p className="developer">{aotw.developer}</p>
