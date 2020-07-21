e278456d471d2e66839c78efe707cc8df85d475f
Ayomide Adekunle <davidadekunle88@gmail.com>
2020-07-21 16:47:26 +0000

    Fixes comments.

--- client/src/Header/header.js
+++ client/src/Header/header.js
@@ -4,6 +4,7 @@ import './header.style.css';
 
 class Header extends Component {
 <<<<<<< HEAD
+<<<<<<< HEAD
     render() {
         return (
             <div className="header">
@@ -12,6 +13,10 @@ class Header extends Component {
 =======
     render(){
         return(
+=======
+    render() {
+        return (
+>>>>>>> Fixes comments.
             <div className="header">
                 <ul className="nav-bar">
                     <li className="nav-element" id="logo">
@@ -21,6 +26,7 @@ class Header extends Component {
                         </a>
                     </li>
 <<<<<<< HEAD
+<<<<<<< HEAD
                     <li className="nav-element link">
                         <a href="https://play.google.com/store/apps/category/GAME">
                             Games
@@ -29,39 +35,58 @@ class Header extends Component {
                     <li className="nav-element active">
 =======
                     <li className="nav-element" id="link">
+=======
+                    <li className="nav-element link">
+>>>>>>> Fixes comments.
                         <a href="https://bit.ly/30xkTrQ">
                             Games
-                            </a>
+                        </a>
                     </li>
+<<<<<<< HEAD
                     <li className="nav-element" id="active">
 >>>>>>> Sets up header
+=======
+                    <li className="nav-element link" id="active">
+>>>>>>> Fixes comments.
                         <a href="https://play.google.com/store/apps">
                             Apps
                         </a>
                     </li>
 <<<<<<< HEAD
+<<<<<<< HEAD
                     <li className="nav-element link">
 =======
                     <li className="nav-element" id="link">
 >>>>>>> Sets up header
+=======
+                    <li className="nav-element link">
+>>>>>>> Fixes comments.
                         <a href="https://play.google.com/store/movies">
                             Movies & TV
                         </a>
                     </li>
 <<<<<<< HEAD
+<<<<<<< HEAD
                     <li className="nav-element link">
 =======
                     <li className="nav-element" id="link">
 >>>>>>> Sets up header
+=======
+                    <li className="nav-element link">
+>>>>>>> Fixes comments.
                         <a href="https://play.google.com/store/books">
                             Books
                         </a>
                     </li>
 <<<<<<< HEAD
+<<<<<<< HEAD
                     <li className="nav-element link">
                         <a href="https://play.google.com/store/apps/category/FAMILY">
 =======
                     <li className="nav-element" id="link">
+=======
+                    <li className="nav-element link">
+>>>>>>> Fixes comments.
                         <a href="https://play.google.com/store/search?q=kids">
 >>>>>>> Sets up header
                             Kids
