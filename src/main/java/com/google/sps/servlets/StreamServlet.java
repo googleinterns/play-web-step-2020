352acd272c1e3716f3cc319984cbc0e56fb67508
bibi hassan <bibihassan2000@gmail.com>
2020-07-23 21:05:32 +0000

    nit changes

--- src/main/java/com/google/sps/servlets/StreamServlet.java
+++ src/main/java/com/google/sps/servlets/StreamServlet.java
@@ -16,7 +16,6 @@ package com.google.sps.servlets;
 
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
