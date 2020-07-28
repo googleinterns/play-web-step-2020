54de4c8cbd38a0d7fd190d0cff9d25b68d867c39
Essence Cain <evcain@aggies.ncat.edu>
2020-07-28 22:48:38 +0000

    Fix styling

--- client/src/Clusters/RecommendedCategories/recommended_categories.test.js
+++ client/src/Clusters/RecommendedCategories/recommended_categories.test.js
@@ -3,19 +3,19 @@ import { render, wait } from '@testing-library/react';
 import RecommendedCategories from './recommended_categories.js';
 
 const data = {
-    "title": "Browse recommended categories",
-		"subtitle": "Didn't find what you're looking for?",
-		"card": [{
-			"id": "mockCard1",
-			"category": "Health & fitness",
-			"url": "/images/healthAndFitnessIcon.png"
-		}],
-}
+    title: "Browse recommended categories",
+	subtitle: "Didn't find what you're looking for?",
+	    card: [
+        {
+			id: "mockCard1",
+			category: "Health & fitness",
+			url: "/images/healthAndFitnessIcon.png"
+		}
+    ]}
 
 const renderRecommendedCategoriesCluster = () =>
     render(
-        <RecommendedCategories
-            data={data} />
+        <RecommendedCategories data={data} />
     );
 
 test('correct header', async () => {
