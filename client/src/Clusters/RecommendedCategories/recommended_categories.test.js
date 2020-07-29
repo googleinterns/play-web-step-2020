470b69aecda349861688d825bc32f52aa7ed6809
Essence Cain <evcain@aggies.ncat.edu>
2020-07-29 16:46:07 +0000

    Fix indentation

--- client/src/Clusters/RecommendedCategories/recommended_categories.test.js
+++ client/src/Clusters/RecommendedCategories/recommended_categories.test.js
@@ -4,14 +4,14 @@ import RecommendedCategories from './recommended_categories.js';
 
 const data = {
     title: "Browse recommended categories",
-	subtitle: "Didn't find what you're looking for?",
-	    card: [
-        {
-			id: "mockCard1",
-			category: "Health & fitness",
-			url: "/images/healthAndFitnessIcon.png"
-		}
-    ]}
+    subtitle: "Didn't find what you're looking for?",
+    card: 
+    [{
+        id: "mockCard1",
+		category: "Health & fitness",
+		url: "/images/healthAndFitnessIcon.png"
+	}]
+}
 
 const renderRecommendedCategoriesCluster = () =>
     render(
